bb8549ba0beee5f7e2a0aae87b6b33ca0057e92b
leoguignard <leo.guignard@gmail.com>
2022-02-16 15:05:46 +0100

    build gg opti

--- src/sc3D/sc3D.py
+++ src/sc3D/sc3D.py
@@ -447,7 +447,7 @@ class Embryo:
         return M
 
     @staticmethod
-    def build_gabriel_graph(node_ids, pos):
+    def build_gabriel_graph(node_ids, pos, data_struct='adj-dict'):
         """
         Build the gabriel graph of a set of nodes with
         associtated positions.
@@ -456,11 +456,18 @@ class Embryo:
             node_ids ([int, ] (size n)): list of node ids
             pos (n x m ndarray): ndarray of the positions where n is
                 the number of nodes and m is the spatial dimension
+            data_struct (str): in which type of data structure will
+                the graph be saved, currently either 'adj-dict' and
+                'adj-mat' are supported.
+                'adj-dict': Adjacency dictionary
+                'adj-mat' : Adjacency matrix
         Returns:
             final_GG (dict id: set([ids, ])): the gabriel graph as
                 an adjacency list, a dictionary that maps node ids
                 to the list of neighboring node ids
         """
+        if not data_struct in ['adj-dict', 'adj-mat']:
+            raise ValueError('Data structure for the Gabriel graph not understood')
         tmp = Delaunay(pos)
         delaunay_graph = {}
 
@@ -468,19 +475,31 @@ class Embryo:
             for e1, e2 in combinations(np.sort(N), 2):
                 delaunay_graph.setdefault(e1, set()).add(e2)
                 delaunay_graph.setdefault(e2, set()).add(e1)
-        Gabriel_graph = {}
 
-        for e1, neighbs in delaunay_graph.items():
-            for ni in neighbs:
-                if not any(np.linalg.norm((pos[ni] + pos[e1])/2 - pos[i])<np.linalg.norm(pos[ni] - pos[e1])/2
-                           for i in neighbs.intersection(delaunay_graph[ni])):
-                    Gabriel_graph.setdefault(e1, set()).add(ni)
+        if data_struct.lower() == 'adj-dict':
+            Gabriel_graph = {}
+            for e1, neighbs in delaunay_graph.items():
+                for ni in neighbs:
+                    if not any(np.linalg.norm((pos[ni] + pos[e1])/2 - pos[i])<np.linalg.norm(pos[ni] - pos[e1])/2
+                               for i in neighbs.intersection(delaunay_graph[ni])):
+                        Gabriel_graph.setdefault(e1, set()).add(ni)
+                        Gabriel_graph.setdefault(ni, set()).add(e1)
 
-        final_GG = {}
-        for e1, neighbs in Gabriel_graph.items():
-            neighbs = np.array(list(neighbs))
-            distances = np.linalg.norm(pos[e1] - [pos[ni] for ni in neighbs], axis=1)
-            final_GG[node_ids[e1]] = {node_ids[ni] for ni in neighbs[distances<=5*np.median(distances)]}
+            final_GG = {}
+            for e1, neighbs in Gabriel_graph.items():
+                neighbs = np.array(list(neighbs))
+                distances = np.linalg.norm(pos[e1] - [pos[ni] for ni in neighbs], axis=1)
+                final_GG[node_ids[e1]] = {node_ids[ni] for ni in neighbs[distances<=5*np.median(distances)]}
+
+        elif data_struct.lower() == 'adj-mat':
+            X, Y = [], []
+            for e1, neighbs in delaunay_graph.items():
+                for ni in [n for n in neighbs if e1<n]:
+                    if not any(np.linalg.norm((pos[ni] + pos[e1])/2 - pos[i])<np.linalg.norm(pos[ni] - pos[e1])/2
+                               for i in neighbs.intersection(delaunay_graph[ni])):
+                        X.append(node_ids[e1])
+                        Y.append(node_ids[ni])
+            final_GG = sp.sparse.coo_array(([True]*len(X), (X, Y)), shape=(max(node_ids)+1, max(node_ids)+1))
         return final_GG
 
     def plot_coverslip(self, cs, pos='pos', ax=None,
@@ -1344,7 +1363,7 @@ class Embryo:
                 the tissue `t_id`. The main value is in the column `Distance_to_reg`
         """
         cells = list(self.all_cells)
-        pos_3D = [np.array(list(self.final[c])+[self.z_pos[c]]) for c in cells]
+        pos_3D = [self.pos_3D[c] for c in cells]
 
         if self.full_GG is None:
             self.full_GG = self.build_gabriel_graph(cells, pos_3D)
