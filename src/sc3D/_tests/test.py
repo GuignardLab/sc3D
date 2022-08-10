97d5cb255c364ae32aa3e46cb6c2c6632f4e884e
Léo Guignard <leo.guignard@univ-amu.fr>
2022-08-10 16:22:49 +0200

    'tests'

--- src/sc3D/_tests/test.py
+++
@@ -1,4 +0,0 @@
-from sc3D import Embryo
-
-def test_sc3D():
-    em = Embryo()
---
+++ src/sc3D/_tests/test_sc3D.py
@@ -0,0 +1,9 @@
+from ast import If
+from sc3D import Embryo
+
+def test_sc3D():
+    em = Embryo('data/data_test.h5ad')
+    print('hello')
+
+if __name__=='__main__':
+    test_sc3D()
